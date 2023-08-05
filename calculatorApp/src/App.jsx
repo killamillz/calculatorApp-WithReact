2003c437c2b59da403d4caf51deab9e49a09a277
ogochukwu alzoie <ogochukwu.alozie@decagon.dev>
2023-08-05 10:36:14 +0100

    finally done

--- calculatorApp/src/App.jsx
+++ calculatorApp/src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-fallthrough */
 /* eslint-disable react/jsx-no-undef */
 import DigitButton from "./digitButton"
@@ -18,6 +19,13 @@ export const ACTIONS = {
 function reducer(state, { type, payload }) {
      switch(type){
           case ACTIONS.ADD_DIGIT:
+               if (state.overwrite){
+                    return {
+                         ...state,
+                         currentOperand: payload.digit,
+                         overwrite: false,
+                    }
+               }
                if(payload.digit === "0" && state.currentOperand === "0"){ 
                     return state
                }
@@ -32,6 +40,13 @@ function reducer(state, { type, payload }) {
                case ACTIONS.CHOOSE_OPERATION:
                     if(state.currentOperand == null && state.previousOperand == null){
                          return state
+                    }
+
+                    if (state.currentOperand == null){
+                         return{
+                              ...state,
+                              operation: payload.operation
+                         }
                     }
                     if (state.previousOperand == null) {
                          return {
@@ -50,6 +65,43 @@ function reducer(state, { type, payload }) {
                     }
                case ACTIONS.CLEAR:
                     return {}
+               case ACTIONS.DELETE_DIGIT:
+                    if(state.overwrite){
+                         return {
+                              ...state,
+                              overwrite: false,
+                              currentOperand: null
+                         }
+                    }
+
+                    if (state.currentOperand == null) return state
+                    if(state.currentOperand.length === 1) {
+                         return {
+                              ...state, currentOperand: null
+                         }
+                    } 
+
+                    return {
+                         ...state,
+                         currentOperand: state.currentOperand.slice(0, -1)
+                    }
+
+               case ACTIONS.EVALUATE:
+                    if (
+                         state.operation == null ||
+                         state.currentOperand == null ||
+                         state.previousOperand == null
+                    ){
+                         return state
+                    }
+
+                    return {
+                         ...state,
+                         overwrite: true,
+                         previousOperand: null,
+                         operation: null,
+                         currentOperand: evaluate(state),
+                    }
      }
 }
 
@@ -73,6 +125,20 @@ function evaluate({ currentOperand, previousOperand, operation}) {
      return computation.toString()
 }
 
+
+// eslint-disable-next-line no-undef
+const INTEGER_FORMATTER = new Int1.NumberFornat("en-us", {
+     maximumFractionDigits: 0, 
+})
+
+function formatOperand(operand) {
+     if(operand == null) return
+     const [integer, decimal] = operand.spilt('.')
+     if(decimal == null) return INTEGER_FORMATTER.fornat(integer)
+     return `${INTEGER_FORMATTER.fornat(integer)}.${decimal}`
+}
+
+
 function App() {
      const [ { currentOperand, previousOperand, operation }, dispatch] = useReducer(reducer,{})
 
@@ -82,11 +148,11 @@ function App() {
      
     <div className="calculator-grid">
           <div className='output'> 
-                <div className='previous-operand' > {previousOperand} {operation}</div>
-                <div className="current-operand">{currentOperand}</div>
+                <div className='previous-operand' > {formatOperand(previousOperand)} {operation}</div>
+                <div className="current-operand">{formatOperand(currentOperand)}</div>
           </div>
           <button className='span-two' onClick={() => dispatch({ type: ACTIONS.CLEAR })}>AC</button>
-          <button>DEL</button>
+          <button onClick={() => dispatch({ type: ACTIONS.DELETE_DIGIT })}>DEL</button>
           <OperationButton operation="÷" dispatch={dispatch} />
           <DigitButton digit="1" dispatch={dispatch} />
           <DigitButton digit="2" dispatch={dispatch} />
@@ -102,7 +168,7 @@ function App() {
           <OperationButton operation="-" dispatch={dispatch} />
           <DigitButton digit="." dispatch={dispatch} />
           <DigitButton digit="0" dispatch={dispatch} />
-          <button className='span-two'>=</button>
+          <button className='span-two' onClick={() => dispatch({ type: ACTIONS.EVALUATE })}>=</button>
     </div>
     </>
   )
